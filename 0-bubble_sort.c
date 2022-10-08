f375bd38009e314e27e1c23111351bc4518d22b4
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-08 05:38:49 +0100

    Created the first implementation for bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of @array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	char tmp;
+	size_t i, swapped = 1;
+
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 1; i < size; i++)
+		{
+			if  (array[i - 1] > array[i])
+			{
+				tmp = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = tmp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+	}
+}
