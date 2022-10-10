dbef7a782b6e0a669f66a576be5eea2a2d6b6c69
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-10 05:58:26 +0100

    Resolved case, `argument == NULL`

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr, *temp, *forward = *list;
+	listint_t *curr, *temp, *forward;
 
 	if (list == NULL || (*list)->next == NULL)
 		return;
