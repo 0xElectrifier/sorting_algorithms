3fd6b059b99ec381b4a4a43de8db346c8f8171e1
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-10 19:12:28 +0100

    First testing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,13 +3,12 @@
 /**
  * partition - implements the `Lomuto partition scheme`
  * @array: array to sort
- * @size: size of @array
  * @low: the lower bound of the @array, usually 0
  * @high: the higher bound of @array, usually length(array) - 1
  *
  * Return: the pivot index
  */
-int partition(int *array, size_t size, size_t low, size_t high)
+int partition(int *array, size_t low, size_t high)
 {
 	int pivot, i, j, temp;
 
@@ -25,12 +24,16 @@ int partition(int *array, size_t size, size_t low, size_t high)
 			array[j] = array[i];
 			array[i] = temp;
 
-			print_array(array, size);
+			print_array(array, SIZE);
 		}
-		
 	}
 
-	return ();
+	i++;
+	temp = pivot;
+	array[high - 1] = array[i];
+	array[i] = temp;
+
+	return (i);
 }
 
 
@@ -48,14 +51,18 @@ void quick_sort(int *array, size_t size)
 	size_t low, high;
 	int p, *array2;
 
+	#ifndef SIZE
+	#define SIZE size
+	#endif
+
 	low = 0;
 	high = size - 1;
 	p = partion(array, size, low, high);
 	if (p == high)
 		return;
 
-	array2 = (int *)&array[p+1];
+	array2 = (int *)&array[p + 1];
 
 	quick_sort(array, p + 1);
-	quick_sort(array2, size - (p+1));
+	quick_sort(array2, size - (p + 1));
 }
