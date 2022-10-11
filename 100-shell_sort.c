020dc169188d100e4a979626f91f34bab948e779
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-11 17:34:45 +0100

    Implemented the `shell_sort` algorithm, using the `Knuth_Sequence`

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order, using the
+ *		`Shell sort` algorithm, using the `Knuth sequence`
+ * @array: array to be sorted
+ * @size: length of @array
+ *
+ * Return: nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int j, t;
+	size_t i, incr;
+
+	incr = 1;
+	while (incr <= (size - 1)/9)
+		incr = (3 * incr) + 1;
+
+	while (incr >= 1)
+	{
+		for (i = incr; i < size; i++)
+		{
+			t = array[i];
+			for (j = (i - incr); j >= 0 && t < array[j]; j -= incr)
+				array[j + incr] = array[j];
+
+			array[j + incr] = t;
+		}
+		incr /= 3;
+		print_array(array, size);
+	}
+}
